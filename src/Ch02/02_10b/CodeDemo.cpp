b284277c777f160d34277979168257c6a8fba6e9
b3agz <38009915+b3agz@users.noreply.github.com>
2026-05-25 10:09:14 +0000

    Completed 02_10b

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -5,8 +5,26 @@
 #include <iostream>
 #include <cstdint>
 
+template <typename T>
+void prntln(T content) {
+    std::cout << content << std::endl << std::endl;
+}
+
 int main(){
+
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+
+    prntln(flt);
+    prntln(sgn);
+    prntln((int32_t)unsgn);
     
     std::cout << std::endl << std::endl;
     return (0);
+
 }
