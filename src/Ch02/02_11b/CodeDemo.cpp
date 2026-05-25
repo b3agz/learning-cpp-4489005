59da73c5295de156675c75e841f7531eb94b6fb2
b3agz <38009915+b3agz@users.noreply.github.com>
2026-05-25 10:17:52 +0000

    Completed 02_11b

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -5,8 +5,32 @@
 #include <iostream>
 #include <cstdint>
 
-int main(){
-    
+template <typename T>
+void prntln(T content) {
+    std::cout << content << std::endl << std::endl;
+}
+void newln() {
     std::cout << std::endl << std::endl;
+}
+
+int main(){
+
+    int fahrenheight = 100;
+    int celsius;
+
+    celsius = ((float)5 / 9.0) * (fahrenheight - 32);
+
+    newln();
+    prntln(celsius);
+    prntln(fahrenheight);
+
+    float weight = 10.99;
+
+    newln();
+    prntln(weight);
+    prntln((int)weight);
+    prntln((int)((weight - (int)weight) * 10000));
+    
+    newln();
     return (0);
 }
